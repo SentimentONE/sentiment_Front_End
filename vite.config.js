4732ec56f60b8fa57c83ddbc999d3e74153f7f5f
Wandrey <123254136+wandrey7@users.noreply.github.com>
2026-01-08 19:15:05 +0000

    feat: minor changes

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,11 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 4173,
+    allowedHosts: ['sentimentry.onrender.com'],
   }
 })
 
